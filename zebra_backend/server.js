6434b88bf68369ea968a606787614db0660c8c6e
Josh Kaplan <josh@joshkaplan.com>
2020-04-18 00:56:29 -0700

    Backend now receives webhooks
    Added basic code to receive web hooks, forwarded from the Stripe CLI. Doesn't actually do anything with the requests yet.

--- zebra_backend/server.js
+++ zebra_backend/server.js
@@ -1,12 +1,22 @@
-const express = require('express')
+const argv = require('yargs').argv;
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 5000
 
+if(!argv.key) {
+    console.log('Please specify a Stripe API key when running: --key=<key_here>');
+
+    // Quit with invalid process exit code
+    process.exit(9);
+}
+
 // Set your secret key. Remember to switch to your live secret key in production!
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('sk_test_KdE2iNsDxi481yEX9aO4KkQr00aSeeoINj');
+// Key = sk_test_KdE2iNsDxi481yEX9aO4KkQr00aSeeoINj
+const stripe = require('stripe')(argv.key);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('What\'re you looking at this? This is an API server!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
@@ -20,6 +30,40 @@ app.get('/api/secret', async(req, res) => {
     res.json({client_secret: intent.client_secret});
 });
 
-app.get('/api/not_secret', async(req, res) => {
-    res.json({hello: 'world'});
-});
+app.post('/hooks', bodyParser.raw({type: 'application/json'}), (req, res) => {
+    console.log("webhook received");
+    
+    let event;
+
+    try {
+        event = JSON.parse(req.body);
+    } catch (err) {
+        res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    
+    console.log("Event type: " + event.type);
+
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+        case 'payment_method.attached':
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+        // ... handle other event types
+        default:
+        // Unexpected event type
+        return res.status(400).end();
+    }
+
+    // Return a response to acknowledge receipt of the event
+    res.json({received: true});
+
+    //console.log(req);
+    //res.status(200).end()
+})
