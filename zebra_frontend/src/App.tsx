03ef36e558053462a059bd402e81738e9c990b37
Josh Kaplan <josh@joshkaplan.com>
2020-04-21 17:30:13 -0700

    Frontend: App now fetches deal of the day info
    Data now fetched in App, some of it passed to DealOfTheDay. This allows for not showing the buy button until/if data has been succesfully fetched from the server. Also moved the price of the daily deal item into the buy button.

--- zebra_frontend/src/App.tsx
+++ zebra_frontend/src/App.tsx
@@ -5,34 +5,99 @@ import DealOfTheDay from './DealOfTheDay';
 
 interface AppState {
     showCheckoutForm: boolean;
+    dealOfTheDayInfoFetched: boolean;
+    name: string;
+    description: string;
+    imageUrl: string; 
+    price: number;
+    currency: string;
+}
+
+/**
+ * Information returned by the server describing the deal of the day
+ */
+interface DealOfTheDayInfo {
+  name: string;
+  description: string;
+  image_url: string; 
+  price: number;
+  currency: string;
 }
 
 class App extends Component<{}, AppState> {
     constructor(props: {}) {
         super(props);
 
-        this.state = {showCheckoutForm: false};
+        this.state = {showCheckoutForm: false,
+                      dealOfTheDayInfoFetched: false,
+                      name: '',
+                      description: '',
+                      imageUrl: '', 
+                      price: 0,
+                      currency: ''};
         this.handleButtonClick = this.handleButtonClick.bind(this);
     }
 
     render() {
-        return (
+        if(this.state.dealOfTheDayInfoFetched) {
+        let buyNow: string = "Buy now for $" + this.state.price / 100;  
+
+          return (
             <div className="App">
                 <header className="App-header">
                     <h2>Your Striped Deal of the Day</h2>
-                    <DealOfTheDay/>
+                    <DealOfTheDay name={this.state.name}
+                                  description={this.state.description}
+                                  imageUrl={this.state.imageUrl}
+                                  price={this.state.price}/>
                     {this.state.showCheckoutForm ?
                         <CheckoutForm/> :
-                        <button className="App-button" onClick={this.handleButtonClick}>Buy now</button>}
+                        <button className="App-button" onClick={this.handleButtonClick}>{buyNow}</button>}
                 </header>
             </div>
           );
+        }
+        else
+        {
+          return (
+            <div className="App">
+                <header className="App-header">
+                    <h2>Your Striped Deal of the Day</h2>
+                    <div className="App-loading">Loading...</div>
+                </header>
+            </div>
+          );
+        }
+
     }
 
-  handleButtonClick(event: MouseEvent) {
-      event.preventDefault();
-      this.setState({showCheckoutForm: true});
-  }
+    handleButtonClick(event: MouseEvent) {
+        event.preventDefault();
+        this.setState({showCheckoutForm: true});
+    }
+
+    componentDidMount() {
+        this.fetchDealOfTheDayInfo();
+    }
+
+    /**
+     * Async fetches information from the server about the deal
+     * of the day and sets this component's state to it
+     */
+    async fetchDealOfTheDayInfo() {
+        let response = await fetch('/api/dotd');
+        if(response.status === 200) {
+            let data: DealOfTheDayInfo = await response.json();
+            this.setState({
+                dealOfTheDayInfoFetched: true,
+                name: data.name,
+                description: data.description,
+                imageUrl: data.image_url,
+                price: data.price,
+                currency: data.currency
+            });
+        }
+    }
 }
 
 export default App;
--- zebra_frontend/src/DealOfTheDay.tsx
+++ zebra_frontend/src/DealOfTheDay.tsx
@@ -1,56 +1,26 @@
 import React, { Component } from 'react';
 import './DealOfTheDay.css';
 
-/**
- * Information returned by the server describing the deal of the day
- */
-interface DealOfTheDayState {
+interface DealOfTheDayProps {
     name: string;
     description: string;
-    image_url: string; 
+    imageUrl: string; 
     price: number;
-    currency: string;
 }
 
-class DealOfTheDay extends Component<{}, DealOfTheDayState> {
+class DealOfTheDay extends Component<DealOfTheDayProps> {
 
     render() {
-        // If state data has been fetched from server, render it
-        if(this.state) {
-            let priceStr: string = "$" + this.state.price / 100;
+        let priceStr: string = "$" + this.props.price / 100;
 
-            return (
-                <div className="DealOfTheDay">
-                    <img className="DealImage" src={this.state.image_url} alt={this.state.description} />
-                    <div className="DealOfTheDay-name">{this.state.name}</div>
-                    <div className="DealOfTheDay-price">{priceStr}</div>
-                </div>
-            );
-        }
-        // Otherwise show the user it's loading
-        else {
-            return (
-                <div className="DealOfTheDay-message">
-                  Loading your deal of the day...
-                </div>
-            );
-        }
+        return (
+            <div className="DealOfTheDay">
+                <img className="DealImage" src={this.props.imageUrl} alt={this.props.description} />
+                <div className="DealOfTheDay-name">{this.props.name}</div>
+            </div>
+        );
     }
 
-    componentDidMount() {
-        this.fetchDealOfTheDay();
-    }
-
-    /**
-     * Async fetches information from the server about the deal
-     * of the day and sets this component's state to it
-     */
-    async fetchDealOfTheDay() {
-        let response = await fetch('/api/dotd');
-        if(response.status === 200) {
-            this.setState(await response.json());
-        }
-    }
 }
 
 export default DealOfTheDay;
