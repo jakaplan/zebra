294983a4be26dd9a4a6b400bb7a8dafc567cfad4
Josh Kaplan <josh@joshkaplan.com>
2020-04-21 18:14:56 -0700

    Frontend: read publishable key from environment variable

--- zebra_frontend/src/index.tsx
+++ zebra_frontend/src/index.tsx
@@ -5,13 +5,25 @@ import {loadStripe} from '@stripe/stripe-js';
 import './index.css';
 import App from './App';
 
-const stripePromise = loadStripe("pk_test_l92KTAp5cyTt29TlW3TLepjl00cKJoqTNq");
+// Read the publishable key set as an environment variable
+let publishableKey = process.env.REACT_APP_PUBLISHABLE_KEY;
+if(publishableKey) {
+  const stripePromise = loadStripe(publishableKey);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Elements stripe={stripePromise}>
-      <App />
-    </Elements>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <React.StrictMode>
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <div>Set publishable key; in your terminal type: export REACT_APP_PUBLISHABLE_KEY=&lt;publishable_key&gt;</div>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
